0f915400579b80f00cbd24e032280dc5bb38d2bf
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2025-05-01 15:05:30 +0000

    chore: init project

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  extends: ['jira'],
-  plugins: ['commitlint-plugin-jira-rules'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'jira-task-id-project-key': [2, 'always', ['MOB']],
-    'jira-task-id-separator': [2, 'always', '-'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build', // Alterações que afetam o sistema de build ou dependências
+        'chore', // Tarefas de manutenção, não relacionadas ao código do usuário
+        'ci', // Alterações em arquivos/configurações de CI
+        'docs', // Documentação
+        'feat', // Novas funcionalidades
+        'fix', // Correção de bugs
+        'perf', // Melhorias de performance
+        'refactor', // Refatoração que não corrige bug nem adiciona feature
+        'revert', // Reverte um commit anterior
+        'style', // Formatação, falta de ponto e vírgula, etc (não altera código)
+        'test', // Adicionando testes faltantes
+      ],
+    ],
   },
 };
